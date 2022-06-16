fec80920da25c9cc38520d142f64625e415dcd70
Oleksandr Humeniuk <oleksandr_humeniuk@epam.com>
2022-06-16 10:37:12 +0300

    modify star_HW_1_JS.js and add an index.html

--- star_HW_1_JS.js
+++ star_HW_1_JS.js
@@ -7,11 +7,83 @@
 // }
 // Вывести в консоль результат работы функции с возрастами 17, 18, 61
 
+// const checkAge = function (age) {
+//     let age_2 = 18;
+//     let age_3 = 60;
+//     if (age < age_2) {
+//         console.log("You don't have access cause your age is " + age + " It's less then")
+//     } else if (age >= age_2 && age < age_3) {
+//         console.log("Welcome!");
+//     } else if (age > age_3) {
+//         console.log("Keep calm and look Culture channel");
+//     } else {
+//         console.log("Technical work");
+//     }
+// }
+// checkAge(60);
+
 // 2*:
 // Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+
+// const checkAge = function (age) {
+//     let age_2 = 18;
+//     let age_3 = 60;
+//     if (!Number(age)) {
+//         console.log("The age is not a number");
+//     } else if (age < age_2) {
+//         console.log("You don't have access cause your age is " + age + " It's less then");
+//     } else if (age >= age_2 && age < age_3) {
+//         console.log("Welcome!");
+//     } else if (age > age_3) {
+//         console.log("Keep calm and look Culture channel");
+//     } else {
+//         console.log("Technical work ");
+//     }
+// }
+
+// checkAge("hello");
 
 // 3**:
 // Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
 
+// const checkAge = function (age) {
+//     age = +age;
+//     let age_2 = 18;
+//     let age_3 = 60;
+//     if (!Number(age)) {
+//         console.log("The age is not a number");
+//     } else if (age < age_2) {
+//         console.log("You don't have access cause your age is " + age + " It's less then");
+//     } else if (age >= age_2 && age < age_3) {
+//         console.log("Welcome!");
+//     } else if (age > age_3) {
+//         console.log("Keep calm and look Culture channel");
+//     } else {
+//         console.log("Technical work");
+//     }
+// }
+
+// checkAge("hello");
+
+
 // 4***:
 // Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
+
+const checkAge = function (age) {
+    age = +age;
+    let age_2 = 18;
+    let age_3 = 60;
+    if (!Number(age)) {
+        alert("The age is not a number");
+    } else if (age < age_2) {
+        alert("You don't have access cause your age is " + age + " It's less then");
+    } else if (age >= age_2 && age < age_3) {
+        alert("Welcome!");
+    } else if (age > age_3) {
+        alert("Keep calm and look Culture channel");
+    } else {
+        alert("Technical work");
+    }
+}
+
+checkAge(prompt("Enter your age: "));
